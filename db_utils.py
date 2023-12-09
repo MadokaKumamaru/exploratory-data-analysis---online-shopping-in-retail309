5812a0c4155cdcfd4a7b6fc3424c9e8ae8ed6af1
MadokaKumamaru <madoka.london2011@gmail.com>
2023-12-09 17:27:10 +0000

    db_utils.py updated

--- db_utils.py
+++ db_utils.py
@@ -5,34 +5,33 @@ import pandas as pd
 
 # Create a function which loads the yaml credentials file and return 
 # the data dictionary contained within
-def import_yaml(yaml_filename):
-    with open('yaml_filename', 'r') as file:
+def import_yaml():
+    with open('credentials.yaml', 'r') as file:
         return yaml.safe_load(file)
+yaml_dictionary = import_yaml()    
 
 # Create a class which will contain the methods we will use to 
 # extract data from the RDS database
 class RDSDatabaseConnector:
-    def __init__(self, yaml_filename):
-        self.yaml_filename = yaml_filename
-        self.yaml_dictionary = import_yaml(self.yaml_filename)
+    def __init__(self, yaml_dictionary):
+        self.yaml_dictionary = yaml_dictionary
         
-        # Define a method which initialises a SQLAlchemy engine 
-        # from the credentials provided to the class
-        def create_engine(self):
-            DATABASE_TYPE = 'postgresql'
-            DBAPI = 'psycopg2'
-            ENDPOINT = self.yaml_dictionary['RDS_HOST']
-            USER = self.yaml_dictionary['RDS_USER']
-            PASSWORD = self.yaml_dictoinary['RDS_PASSWORD']
-            PORT = self.yaml_dictionary['RDS_PORT']
-            DATABASE = self.yaml_dictionary['RDS_DATABASE']
-            engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
-            engine.connect()
+    # Define a method which initialises a SQLAlchemy engine 
+    # from the credentials provided to the class
+    def connect_database(self):
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        ENDPOINT = self.yaml_dictionary['RDS_HOST']
+        USER = self.yaml_dictionary['RDS_USER']
+        PASSWORD = self.yaml_dictionary['RDS_PASSWORD']
+        PORT = self.yaml_dictionary['RDS_PORT']
+        DATABASE = self.yaml_dictionary['RDS_DATABASE']
+        return create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
             
-        # Define a method which extracts data from the RDS database
-        # and returns it as a Pandas DataFrame
-        def extract_data(self):
-            ?????????
+    # Define a method which extracts data from the RDS database
+    # and returns it as a Pandas DataFrame
+    def extract_data(self, engine):
+        return pd.read_sql_table('customer_activity', engine)
             
         
 # Create a function which saves the data in .csv format to local machine
@@ -42,4 +41,9 @@ def save_data():
 # Create a function which will load the data from your local machine
 # into a pandas DataFrame
 def load_data():
-    customer_activity = pd.read_csv('customer_activity.csv')
+    return pd.read_csv('customer_activity.csv')
+    
+test = RDSDatabaseConnector(yaml_dictionary)
+engine = test.connect_database()
+df = test.extract_data(engine)
+print(df.head())
