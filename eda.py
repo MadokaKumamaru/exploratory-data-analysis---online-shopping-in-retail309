43dc1133bc5aa798f4ffe48f674b5bc462a8786f
MadokaKumamaru <madoka.london2011@gmail.com>
2023-12-10 01:19:52 +0000

    updated

--- eda.py
+++ eda.py
@@ -9,5 +9,13 @@ engine = customer_activity_class.connect_database()
 customer_activity = customer_activity_class.extract_data(engine)
 
 # Get the information about the DataFrame
-print(customer_activity.head())
 customer_activity.info()
+
+# Create a class which contains methods that can be appplied
+# to DataFrame columns to handle datatype conversions
+def DataTransform:
+    def __init__(self): 
+        
+    # Define a method which will convert columns to category datatype
+    def convert_to_category(self, column_name):
+         
