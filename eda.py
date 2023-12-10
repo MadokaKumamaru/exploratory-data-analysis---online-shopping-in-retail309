af9aad485ff6eb448f19efe72f60e07adb041922
MadokaKumamaru <madoka.london2011@gmail.com>
2023-12-10 15:09:32 +0000

    updated

--- eda.py
+++ eda.py
@@ -8,14 +8,26 @@ customer_activity_class = db_utils.RDSDatabaseConnector(db_utils.yaml_dictionary
 engine = customer_activity_class.connect_database()
 customer_activity = customer_activity_class.extract_data(engine)
 
-# Get the information about the DataFrame
-customer_activity.info()
+# Get the data type of each column
+print(customer_activity.dtypes)
 
 # Create a class which contains methods that can be appplied
 # to DataFrame columns to handle datatype conversions
-def DataTransform:
-    def __init__(self): 
+class DataTransform:
+    def __init__(self, dataframe): 
+        self.dataframe = dataframe
         
     # Define a method which will convert columns to category datatype
     def convert_to_category(self, column_name):
+        self.dataframe[column_name] = self.dataframe[column_name].astype('category')
          
+
+# Covert data types for columns
+customer_activity_DataTransform = DataTransform(customer_activity)
+columns_to_transform = ['administrative', 'product_related', 'informational', 
+                        'month', 'operating_systems', 'browser', 'region', 
+                        'traffic_type', 'visitor_type']
+for column in columns_to_transform:
+    customer_activity_DataTransform.convert_to_category(column)
+print(customer_activity.dtypes)
+    
